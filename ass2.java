452eee0eafd7cc0975b2b5201c512b3989a1335e
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-02-06 16:39:03 +0530

    Update ass2.java

--- ass2.java
+++ ass2.java
@@ -3,8 +3,8 @@ public class ass2{
 		if(args.length<2) {
 			System.out.println("Usage:parameterInput <name><age>");
 		}
-		String name=args[0];
-		int age=Integer.parseInt[args[1]];
+		String Name=args[0];
+		int Age=Integer.parseInt[args[1]];
 		System.out.println("Hello "+name+ "You are "+age+ "old");
 		}
 	}
