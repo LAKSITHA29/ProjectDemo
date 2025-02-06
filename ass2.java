d30bea7f9d4af57e25e57d5bd9a29be7ff0cfa7c
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-02-06 16:24:13 +0530

    Add files via upload

---
+++ ass2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class JenkinsDEMOproject{
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		String name=sc.nextLine();
+		int age=sc.nextInt();
+		}
+	}
