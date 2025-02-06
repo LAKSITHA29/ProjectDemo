1daae9f7d45c9cd06afbe1d711612afc5bb85f91
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-02-06 16:36:35 +0530

    Update ass2.java

--- ass2.java
+++ ass2.java
@@ -1,8 +1,10 @@
-import java.util.Scanner;
 public class ass2{
 	public static void main(String args[]) {
-		Scanner sc=new Scanner(System.in);
-		String name=sc.nextLine();
-		int age=sc.nextInt();
+		if(args.length<2) {
+			System.out.println("Usage:parameterInput <name><age>");
+		}
+		String name=args[0];
+		int age=Integer.parseInt[args[1]];
+		System.out.println("Hello "+name+ "You are "+age+ "old");
 		}
 	}
