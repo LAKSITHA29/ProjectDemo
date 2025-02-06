70698df7608264e80df5424f98876f14cadaf0aa
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-02-06 16:42:02 +0530

    Update ass2.java

--- ass2.java
+++ ass2.java
@@ -4,7 +4,7 @@ public class ass2{
 			System.out.println("Usage:parameterInput <name><age>");
 		}
 		String Name=args[0];
-		int Age=Integer.parseInt[args[1]];
+		int Age=Integer.parseInt(args[1]);
 		System.out.println("Hello "+name+ "You are "+age+ "old");
 		}
 	}
