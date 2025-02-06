ac1a536f1800ee9736121eaec52998218b736d45
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-02-06 16:28:50 +0530

    Update ass2.java

--- ass2.java
+++ ass2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JenkinsDEMOproject{
+public class ass2{
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
 		String name=sc.nextLine();
