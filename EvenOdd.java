6422820137dc95d2b222b1994a91d76b8e28129a
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-02-07 10:25:37 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,33 @@
+import file.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class EvenOdd{
+	public static void main(String[] args) {
+    try{
+      File file=new File("input.txt");
+      int even=0,odd=0;
+       Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+    for(int i=0;i<n;i++){
+      if(arr[i]%2==0){
+        even++;
+    }else{
+        odd++;
+        }
+    }
+      System.out.println("odd:"+odd+"\neven:"+even+);
+      sc.close();
+    }catch(FileNotFoundException e){
+       System.out.println("Error:File not found.");
+      e.printStackTrace();
+    }
+  }
+}
+      
+      
+   
+	
