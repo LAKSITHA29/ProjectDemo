038ac18f3ff2b0ffe3fd9b691382c31b66fffdeb
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-02-07 11:03:43 +0530

    Update Vote.java

--- Vote.java
+++ Vote.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Vote{
     public static void main(String[] args){
         try{
-        File file=new file("input.txt");
+        File file=new File("input.txt");
         Scanner sc=new Scanner(file);
         int age=sc.nextInt();
         if (age>=18) {
